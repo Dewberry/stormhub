456cad50615b75aa64145df7d9af0ca5b2862fa0
Stevenray Janke <stevenrayjanke@yahoo.com>
2025-02-25 14:34:08 -0500

    Clean

--- workflows/gages.py
+++ workflows/gages.py
@@ -1,29 +1,21 @@
-import numpy as np
-import pandas as pd
-import scipy.stats as stats
-from dataretrieval import nwis
-import geopandas as gpd
-from stormhub.hydro.usgs.usgs import new_gage_collection
-from stormhub.hydro.plots import plot_log_pearson_iii
+from stormhub.hydro.usgs.usgs import new_gage_collection, new_gage_catalog
+from stormhub.hydro.utils import find_gages_in_watershed
 from stormhub.logger import initialize_logger
 
-watershed = "C:\\Users\\sjanke\\Code\\stormhub\\bighorn\\bighorn.geojson"
 
-def find_gages_in_watershed(watershed):
+if __name__ == "__main__":
 
-    watershed = gpd.read_file(watershed)
-    bbox = [round(coord, 6) for coord in watershed.total_bounds.tolist()]
+    initialize_logger()
+    catalog_id = "gage_catalog"
+    local_directory = "C:\\Users\\sjanke\\Code\\stormhub\\usgs_gages"
+    watershed = "C:\\Users\\sjanke\\Code\\stormhub\\bighorn\\bighorn.geojson"
 
-    gages_in_watershed_bbox = nwis.get_info(bBox=bbox)[0]
+    gage_catalog = new_gage_catalog(
+        catalog_id,
+        local_directory=local_directory,
+        catalog_description="Bighorn Catalog",
+    )
 
-    watershed_geom = watershed.iloc[0].geometry
-    gages_within_watershed = gages_in_watershed_bbox[gages_in_watershed_bbox.within(watershed_geom)]
-    filtered_gages = gages_within_watershed[gages_within_watershed['site_no'].str.len() == 8]
-    return(filtered_gages["site_no"].to_list())
-
-
-initialize_logger()
-
-gage_numbers = ["12105900", "12167000"] # Make this a list, created by a watershed function in hydro.utils
-dir = "C:\\Users\\sjanke\\Code\\stormhub\\usgs_gages"
-new_gage_collection(gage_nums, dir)
+    # gage_numbers = ["12105900", "12167000"]
+    gage_numbers = find_gages_in_watershed(watershed, 15)
+    new_gage_collection(gage_catalog, gage_numbers, local_directory)
